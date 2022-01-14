3b36a3e31703a59f1fd99bb9b978c349597b93a3
xp <xp@CETYSDAM00>
2022-01-14 19:21:11 +0100

    Escalera palabras iniciado

---
+++ src/ejerciciosnivel02/EjerciciosNivel02.java
@@ -0,0 +1,35 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package ejerciciosnivel02;
+
+/**
+ *
+ * @author Alex Cobos López
+ */
+public class EjerciciosNivel02 {
+    
+    public boolean escaleraPalabras(char[][] listaPalabras){
+        return true;
+    }
+    
+    
+    
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        char [][] listaPalabras = {
+            {'P', 'A', 'T', 'A'}, 
+            {'P', 'A', 'T', 'O'}, 
+            {'R', 'A', 'T', 'O'}, 
+            {'R', 'A', 'M', 'O'}, 
+            {'G', 'A', 'M', 'O'}, 
+            {'G', 'A', 'T', 'O'}, 
+            {'M', 'A', 'T', 'O'},
+        };
+    }
+    
+}
