1167a7d641f050832a6b83fcee74919ae823f543
xp <xp@CETYSDAM00>
2022-01-21 20:04:53 +0100

    Arreglar RPN

--- src/ejerciciosnivel02/EjerciciosNivel02.java
+++ src/ejerciciosnivel02/EjerciciosNivel02.java
@@ -19,7 +19,6 @@ public class EjerciciosNivel02 {
                 if(listaPalabras[i][j] != listaPalabras[i+1][j]){
                     contador++;
                 }
-                
             }
             if (contador != 1){//si es distinto de uno return false
                                //porque se diferencian en más de un caracter
@@ -28,6 +27,107 @@ public class EjerciciosNivel02 {
             
         }
         return true;
+    }
+    
+    public boolean escaleraPalabras2(char[][] lista){
+        for (int j=0; j<lista.length-1; j++){
+            int contador = 0;
+            for (int i=0; i<lista[0].length; i++){
+                if(lista[j][i] != lista[j+1][i]){
+                    contador++;
+                }
+            }
+            System.out.println(contador);
+        
+            if (contador != 1){//si es distinto de uno return false
+                               //porque se diferencian en más de un caracter
+                    System.out.println("No hay escalera de palabras");
+                    return false;
+            }
+        }
+        return true;  
+    }
+    
+    private int cadenaADN(String uno, String dos){
+        int coste = 0;
+        
+        for(int i=0; i<uno.length();i++){
+            if(uno.charAt(i)=='-' || dos.charAt(i)== '-'){
+                coste+= 2;
+            }else{
+            
+            if(uno.charAt(i)=='C' && dos.charAt(i)!='G'){
+                coste++;
+            }
+            
+            if(uno.charAt(i)=='G' && dos.charAt(i)!='C'){
+                coste++;
+            }
+            
+            if(uno.charAt(i)=='A' && dos.charAt(i)!='T'){
+                coste++;
+            }
+            
+            if(uno.charAt(i)=='T' && dos.charAt(i)!='A'){
+                coste++;
+            }
+            }         
+        }
+        return coste;
+    }
+    
+    
+    public int strStr(String uno, String dos){
+        int posicion = -1;
+        
+        for(int i=0; i<uno.length();i++){
+            if(uno.charAt(i)==dos.charAt(0)){
+                posicion = i;
+                int j=0;
+                while(j<dos.length() && i<uno.length() && uno.charAt(i) == dos.charAt(j)){
+                    i++;
+                    j++;
+                }
+                if (j == dos.length()){//ha encontrado la palabra del string 2
+                    return posicion;
+                }
+                if(i == uno.length()){//se ha terminado la palabra 1 y no cabe la dos
+                    return -1;
+                }
+                //si llega aquí es porque ha salido del while
+                //porque las letras son distintas
+                i = posicion;
+                posicion = -1;   
+            }  
+        }
+        return posicion;
+    }
+    
+    public String RPN(String [] entrada){
+        String [] pila = new String [100];
+        int posicion = 0;
+        
+        for( int i=0; i<entrada.length; i++){
+            if(entrada[i].equals("+")){
+                Double.valueOf(pila[posicion]);
+                pila[posicion-2] = String.valueOf(Double.valueOf(pila[posicion-1]) +  Double.valueOf(pila[posicion-2]));
+            }
+            if(entrada[i].equals("-")){
+                pila[posicion-2] = String.valueOf(Double.valueOf(pila[posicion-1]) -  Double.valueOf(pila[posicion-2]));
+            }
+            if(entrada[i].equals("*")){
+                pila[posicion-2] = String.valueOf(Double.valueOf(pila[posicion-1]) *  Double.valueOf(pila[posicion-2]));
+            }
+            if(entrada[i].equals("/")){
+                pila[posicion-2] = String.valueOf(Double.valueOf(pila[posicion-1]) /  Double.valueOf(pila[posicion-2]));
+            }
+            else{
+                pila[posicion] = entrada[i];
+                posicion++;
+            }
+        }
+        
+        return pila[0];
     }
     
     
@@ -48,7 +148,15 @@ public class EjerciciosNivel02 {
         EjerciciosNivel02 e = new EjerciciosNivel02();
         e.escaleraPalabras(listaPalabras);
     System.out.println(e.escaleraPalabras(listaPalabras));
+    System.out.println(e.escaleraPalabras2(listaPalabras));
+    System.out.println(e.cadenaADN("ACGTA","TGCA-"));
+    System.out.println(e.strStr("HOLA_MUNDO","DO"));
+        
+        String[] numeros = 
+            {"3","2","+","7","*","15","21","+","-"};
+        System.out.println(e.RPN(numeros));
     }
+    };
    
     
-}
+
